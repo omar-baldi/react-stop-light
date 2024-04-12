6121723dbd025dfd3b9ab9131b48a3db0543ad52
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-12 10:44:18 +0200

    Refactoring of component to include optional timeout for top light colors and initial optional stop light value

--- src/components/StopLight.tsx
+++ src/components/StopLight.tsx
@@ -1,14 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const stopLightsColors = ["red", "yellow", "green"] as const;
 
 type StopLightColor = (typeof stopLightsColors)[number];
 
-export default function StopLight() {
-  const [activeStopLight, setActiveStopLight] = useState<StopLightColor>("red");
+type SpotLightsTimeouts = Record<StopLightColor, number>;
+
+type Props = {
+  startStopLight?: StopLightColor;
+  stopLightTimeouts?: Partial<SpotLightsTimeouts>;
+};
+
+const defaultStopLightTimeouts = {
+  red: 5000,
+  yellow: 500,
+  green: 2000,
+} satisfies SpotLightsTimeouts;
+
+export default function StopLight({
+  startStopLight = "red",
+  stopLightTimeouts = defaultStopLightTimeouts,
+}: Props) {
+  const [activeStopLight, setActiveStopLight] = useState<StopLightColor>(startStopLight);
+
+  const updatedStopLightTimeouts = useMemo(() => {
+    return {
+      ...defaultStopLightTimeouts,
+      ...stopLightTimeouts,
+    };
+  }, [stopLightTimeouts]);
 
   useEffect(() => {
-    const timeoutAmount = 1000;
+    setActiveStopLight(startStopLight);
+  }, [startStopLight]);
+
+  useEffect(() => {
+    const timeoutAmount = updatedStopLightTimeouts[activeStopLight];
 
     const timeout = setTimeout(() => {
       setActiveStopLight((prevStopLight) => {
@@ -22,7 +49,7 @@ export default function StopLight() {
     return () => {
       clearTimeout(timeout);
     };
-  }, [activeStopLight]);
+  }, [activeStopLight, updatedStopLightTimeouts]);
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
