89dd404d31e6a9625d9948b78ce840301cec9d7d
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-13 11:40:48 +0200

    Added tests to assert correct logic when switching colors

--- tests/unit/hooks/useStopLight.test.ts
+++ tests/unit/hooks/useStopLight.test.ts
@@ -1,7 +1,17 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
+import { vi } from "vitest";
 import { useStopLight } from "../../../src/hooks/useStopLight";
 
+vi.useFakeTimers();
+vi.spyOn(global, "setTimeout");
+vi.spyOn(global, "clearTimeout");
+
 describe("useStopLight", () => {
+  beforeEach(() => {
+    vi.clearAllTimers();
+    vi.clearAllMocks();
+  });
+
   it("should custom hook be initialized with default active stop light if none specified", () => {
     const { result } = renderHook(() => useStopLight());
     const activeStopLight = result.current;
@@ -12,5 +22,28 @@ describe("useStopLight", () => {
     const { result } = renderHook(() => useStopLight({ startStopLight: "yellow" }));
     const activeStopLight = result.current;
     expect(activeStopLight).toBe("yellow");
+  });
+
+  describe("Switching colors", () => {
+    it("should switch from 'red' to 'green' after correct timeout value", () => {
+      const { result } = renderHook(() => useStopLight());
+      expect(result.current).toBe("red");
+      act(() => vi.advanceTimersByTime(5000));
+      expect(result.current).toBe("green");
+    });
+
+    it("should switch from 'green' to 'yellow' after correct timeout value", () => {
+      const { result } = renderHook(() => useStopLight({ startStopLight: "green" }));
+      expect(result.current).toBe("green");
+      act(() => vi.advanceTimersByTime(2000));
+      expect(result.current).toBe("yellow");
+    });
+
+    it("should switch from 'yellow' to 'red' after correct timeout", () => {
+      const { result } = renderHook(() => useStopLight({ startStopLight: "yellow" }));
+      expect(result.current).toBe("yellow");
+      act(() => vi.advanceTimersByTime(500));
+      expect(result.current).toBe("red");
+    });
   });
 });
