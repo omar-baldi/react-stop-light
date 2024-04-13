eeb3642558709f0ecdea43c0bbdc79ac07262afe
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-13 11:36:04 +0200

    Installed dependencies for testing stop-light logic

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import path from "path";
+import { defineConfig, mergeConfig } from "vite";
+import viteConfig from "./vite.config";
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      alias: { "@": path.resolve(__dirname, "src") },
+      environment: "happy-dom",
+    },
+  })
+);
